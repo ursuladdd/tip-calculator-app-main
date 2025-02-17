a18f20f5b71c83df66e6dfb3e45ae18cccf385d6
ursuladdd <anaclarasugajimin14@gmail.com>
2025-02-17 18:59:00 -0300

    Tentando fazer da forma mais lenta

---
+++ app.js
@@ -0,0 +1,5 @@
+let valor = document.getElementById("int-valor");
+let pessoa = document.getElementById("int-quanti");
+let custom = document.getElementById("int-custom");
+let reset = document.getElementById("reset");
+
