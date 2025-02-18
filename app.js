ed9d2c776461eca0f7381dc7a32562f8f1b908cf
ursuladdd <anaclarasugajimin14@gmail.com>
2025-02-18 13:42:24 -0300

    Outros planos

--- app.js
+++
@@ -1,5 +0,0 @@
-let valor = document.getElementById("int-valor");
-let pessoa = document.getElementById("int-quanti");
-let custom = document.getElementById("int-custom");
-let reset = document.getElementById("reset");
-
